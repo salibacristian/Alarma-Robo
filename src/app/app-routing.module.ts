5ba0678749810e87f7d7b6d694cfa1683a3b06fa
salibacristian <chriseze21@gmail.com>
2019-10-18 00:13:10 -0300

    limpieza

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,11 +11,7 @@ const routes: Routes = [
     path: 'home',
     loadChildren: './home/home.module#HomePageModule'
   },
-  {
-    path: 'list',
-    loadChildren: './list/list.module#ListPageModule'
-  },
-  { path: 'log-in', loadChildren: './log-in/log-in/log-in.module#LogInPageModule' }
+    { path: 'log-in', loadChildren: './log-in/log-in/log-in.module#LogInPageModule' }
 ];
 
 @NgModule({
