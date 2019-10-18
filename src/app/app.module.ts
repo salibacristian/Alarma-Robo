788e1968ad608c05b2b38b2a91671fbbb9818e9c
salibacristian <chriseze21@gmail.com>
2019-10-18 00:05:29 -0300

    cambio firebaseConfig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,13 +20,13 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule, FirestoreSettingsToken } from '@angular/fire/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBYL-IP-i1vHu1glzb1PDxlRc33n_Qt-_w",
-  authDomain: "myapp3-c56a4.firebaseapp.com",
-  databaseURL: "https://myapp3-c56a4.firebaseio.com",
-  projectId: "myapp3-c56a4",
-  storageBucket: "",
-  messagingSenderId: "405034328954",
-  appId: "1:405034328954:web:7095658f62bb14b0e96cc3"
+  apiKey: "AIzaSyA9FuQw_mTGYgy-4ecv7BKUULl6L8e8RFo",
+  authDomain: "myapp-fcbfd.firebaseapp.com",
+  databaseURL: "https://myapp-fcbfd.firebaseio.com",
+  projectId: "myapp-fcbfd",
+  storageBucket: "myapp-fcbfd.appspot.com",
+  messagingSenderId: "687611359987",
+  appId: "1:687611359987:web:1a78f70e59c9dacfa2bd86"
 };
 
 @NgModule({
